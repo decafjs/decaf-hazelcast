b246f962502fb2dd9ee28b158036261993c7d6ac
Michael Schwartz <mykesx@gmail.com>
2015-03-11 16:41:45 -0700

    testing

---
+++ examples/decaf.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./bower_components/decaf/bin/decaf $@
+
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mschwartz on 3/11/15.
+ */
+
+/*global require, exports */
+
+decaf.extend(exports, {
+    Hazelcast: require('lib/Hazelcast')
+});
